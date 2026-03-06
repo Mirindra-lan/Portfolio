dabb240f9202bc768c0f40e2726c2975f1603054
Mirindra Lucien LANTOSOA <MRD@setex1.lan>
2026-03-06 07:59:57 +0100

    adding metadata for SEO optimisation

---
+++ next-sitemap.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://mirindra-lan.vercel.app', // ton domaine
+  generateRobotsTxt: true,                     // génère robots.txt
+  sitemapSize: 7000,
+  changefreq: 'weekly',                        // fréquence de mise à jour pour le SEO
+  priority: 0.7,
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      'https://mirindra-lan.vercel.app/sitemap.xml',
+    ],
+  },
+};
